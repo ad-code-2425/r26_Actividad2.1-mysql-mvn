c98ee39f6b515014173d8a8b2a780a0c7e4090fc
ad-code-2425 <ad.code.2425@gmail.com>
2025-11-04 16:16:05 +0100

    mysql 1st conn

--- src/main/java/edu/cdm/conexionbd/PrimeiraConexionMySQL.java
+++ src/main/java/edu/cdm/conexionbd/PrimeiraConexionMySQL.java
@@ -8,15 +8,18 @@ public class PrimeiraConexionMySQL {
 
      public static void main(String[] args) {
         try {
-            // Establecemos la conexion con la BD
+            // Establecemos a conexion coa BD
             //MySQL:  jdbc:mysql://host1:33060/db?key1=value1&key2=value2&key3=value3
             //Debería valer calquera das dúas posibles conexións: 
             // Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/empresa",
             //  "root", "abc123.");
 
-             Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/empresa?user=root&password=abc123.",
-             "root", "abc123.");
            
+             Connection conexion = DriverManager.getConnection(
+    "jdbc:mysql://localhost:3306/empresa?user=root&password="
+   
+);
+
             System.out.println("Parabéns! Conecta correctamente con MySQL");
             conexion.close(); // Cerrar conexión
         } catch (SQLException cn) {
